40e40f19b425b54b09afce4dd6e5f909b14a2c01
Mathew Grabau <mgrabau@mathewgrabau.ca>
2018-01-25 20:01:25 -0600

    Updated the active page tracking and handling the clicks

--- WebContent/javascript/BankPortal.js
+++ WebContent/javascript/BankPortal.js
@@ -1,4 +1,17 @@
 var BankPortal = function() {
+	/* attribute to hold the active page */
+	var activePage = ko.observable("Home");
+
+	/* method to set the active page */
+	var setActivePage = function(page) {
+		console.log("Setting the active page to: " + page);
+		activePage(page);
+	};
+
+	/* returns true if parameter matches active page, otherwise false */
+	var isActivePage = function(page) {
+		return activePage() === page;
+	};
 
 	var init = function() {
 		ko.applyBindings(BankPortal);
@@ -7,5 +20,7 @@ var BankPortal = function() {
 	$(init);
 
 	return {
+		setActivePage: setActivePage,
+		isActivePage: isActivePage
 	};
 }();
