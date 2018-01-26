ae40fc7cdf9a1d58418cedc9b1d244925588000b
Mathew Grabau <mgrabau@mathewgrabau.ca>
2018-01-25 20:23:01 -0600

    Added the tabs for the account types.

--- WebContent/javascript/BankPortal.js
+++ WebContent/javascript/BankPortal.js
@@ -2,15 +2,29 @@ var BankPortal = function() {
 	/* attribute to hold the active page */
 	var activePage = ko.observable("Home");
 
+	/* attribute to hold the active tab. */
+	var activeTab = ko.observable('Accounts');
+
 	/* method to set the active page */
 	var setActivePage = function(page) {
 		console.log("Setting the active page to: " + page);
 		activePage(page);
 	};
 
+	/* method to set the active tab */
+	var setActiveTab = function(tab) {
+		console.log('Setting the active tab to: ' + tab);
+		activeTab(tab);
+	};
+
 	/* returns true if parameter matches active page, otherwise false */
 	var isActivePage = function(page) {
 		return activePage() === page;
+	};
+
+	/* returns true if paraemter matches active tab, false otherwise */
+	var isActiveTab = function(tab) {
+		return activeTab() === tab;
 	};
 
 	var init = function() {
@@ -21,6 +35,8 @@ var BankPortal = function() {
 
 	return {
 		setActivePage: setActivePage,
-		isActivePage: isActivePage
+		isActivePage: isActivePage,
+		setActiveTab: setActiveTab,
+		isActiveTab: isActiveTab
 	};
 }();
